607bd100d16e3f31f02d226b043102c750066ee9
Julien Giovinazzo <juliengiovinazzo@gmail.com>
2021-02-01 09:14:54 +0100

    Fix restriction de page en fonction du role

--- www/controllers/admin_home.js
+++ www/controllers/admin_home.js
@@ -9,42 +9,47 @@ router.get('/', async function(req, res) {
         // Not logged in, redirect to login page (aka index)
         res.redirect('/');
     } else {
-        try {
-            let country = req.query.country;
-            let completion_status = req.query.completion_status; 
-            let student_name = req.query.student_name;
-            let promo = req.query.promo;
-            console.log("QUERY :", req.query);
+        let role = await model_users.get_user_role(req.session.user_id);
+        if (role != "admin") {
+            res.redirect('/');
+        } else {
+            try {
+                let country = req.query.country;
+                let completion_status = req.query.completion_status; 
+                let student_name = req.query.student_name;
+                let promo = req.query.promo;
+                console.log("QUERY :", req.query);
 
-            let trip_data = await model_trips.get_trips_filtered(country, completion_status, student_name, promo);
-            trip_data = JSON.parse(JSON.stringify(trip_data));
-            
-            // Determiner si chaque voyage est à venir, en cours ou terminé.
-            trip_data.forEach(function(t) {
-                if (Date.parse(t.start_date) > Date.now()) {
-                    t.upcoming = true;
-                } else if (Date.parse(t.end_date) < Date.now()) {
-                    t.past = true;
-                } else {
-                    t.current = true;
-                }
-            });
+                let trip_data = await model_trips.get_trips_filtered(country, completion_status, student_name, promo);
+                trip_data = JSON.parse(JSON.stringify(trip_data));
+                
+                // Determiner si chaque voyage est à venir, en cours ou terminé.
+                trip_data.forEach(function(t) {
+                    if (Date.parse(t.start_date) > Date.now()) {
+                        t.upcoming = true;
+                    } else if (Date.parse(t.end_date) < Date.now()) {
+                        t.past = true;
+                    } else {
+                        t.current = true;
+                    }
+                });
 
-            let user_data = await model_users.get_user_by_id(req.session.user_id);
-            user_data = JSON.parse(JSON.stringify(user_data));
-            user_data.firstname = user_data.firstname.charAt(0).toUpperCase() + user_data.firstname.slice(1);
-            user_data.lastname  = user_data.lastname .charAt(0).toUpperCase() + user_data.lastname .slice(1);
+                let user_data = await model_users.get_user_by_id(req.session.user_id);
+                user_data = JSON.parse(JSON.stringify(user_data));
+                user_data.firstname = user_data.firstname.charAt(0).toUpperCase() + user_data.firstname.slice(1);
+                user_data.lastname  = user_data.lastname .charAt(0).toUpperCase() + user_data.lastname .slice(1);
 
-            let country_count = await model_trips.get_trip_count_by_country(country, completion_status, student_name, promo);
+                let country_count = await model_trips.get_trip_count_by_country(country, completion_status, student_name, promo);
 
-            res.render('admin_home', {
-                user_data: user_data,
-                trips: trip_data,
-                country_count: JSON.stringify(country_count),
-                form_data: {country, completion_status, student_name, promo}
-            });
-        } catch (err) {
-            console.error(err);
+                res.render('admin_home', {
+                    user_data: user_data,
+                    trips: trip_data,
+                    country_count: JSON.stringify(country_count),
+                    form_data: {country, completion_status, student_name, promo}
+                });
+            } catch (err) {
+                console.error(err);
+            }
         }
     }
 });
--- www/controllers/student_home.js
+++ www/controllers/student_home.js
@@ -9,32 +9,37 @@ router.get('/', async function(req, res) {
         // Not logged in, redirect to login page (aka index)
         res.redirect('/');
     } else {
-        try {
-            let trip_data = await model_trips.get_trips_by_user_id(req.session.user_id);
-            trip_data = JSON.parse(JSON.stringify(trip_data));
-            
-            // Determiner si chaque voyage est à venir, en cours ou terminé.
-            trip_data.forEach(function(t) {
-                if (Date.parse(t.start_date) > Date.now()) {
-                    t.upcoming = true;
-                } else if (Date.parse(t.end_date) < Date.now()) {
-                    t.past = true;
-                } else {
-                    t.current = true;
-                }
-            });
+        let role = await model_users.get_user_role(req.session.user_id);
+        if (role != "student") {
+            res.redirect('/');
+        } else {
+            try {
+                let trip_data = await model_trips.get_trips_by_user_id(req.session.user_id);
+                trip_data = JSON.parse(JSON.stringify(trip_data));
+                
+                // Determiner si chaque voyage est à venir, en cours ou terminé.
+                trip_data.forEach(function(t) {
+                    if (Date.parse(t.start_date) > Date.now()) {
+                        t.upcoming = true;
+                    } else if (Date.parse(t.end_date) < Date.now()) {
+                        t.past = true;
+                    } else {
+                        t.current = true;
+                    }
+                });
 
-            let user_data = await model_users.get_user_by_id(req.session.user_id);
-            user_data = JSON.parse(JSON.stringify(user_data));
-            user_data.firstname = user_data.firstname.charAt(0).toUpperCase() + user_data.firstname.slice(1);
-            user_data.lastname  = user_data.lastname .charAt(0).toUpperCase() + user_data.lastname .slice(1);
-            
-            res.render('student_home', { 
-                user_data: user_data,
-                trips: trip_data
-            });
-        } catch (err) {
-            console.error(err);
+                let user_data = await model_users.get_user_by_id(req.session.user_id);
+                user_data = JSON.parse(JSON.stringify(user_data));
+                user_data.firstname = user_data.firstname.charAt(0).toUpperCase() + user_data.firstname.slice(1);
+                user_data.lastname  = user_data.lastname .charAt(0).toUpperCase() + user_data.lastname .slice(1);
+                
+                res.render('student_home', { 
+                    user_data: user_data,
+                    trips: trip_data
+                });
+            } catch (err) {
+                console.error(err);
+            }
         }
     }
 });
